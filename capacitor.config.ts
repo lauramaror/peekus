1f4267b23970f85c28563fc3b902f1077795ee87
lauramaror <lauramaror@gmail.com>
2022-12-08 13:31:25 +0100

    update for release

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.ura.peekus',
   appName: 'Peekus',
   webDir: 'www',
   bundledWebRuntime: false,
